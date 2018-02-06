67169d8878329407e911316d648eab24e5f3ee0c
lcharleux <ludovic.charleux@univ-smb.fr>
2018-02-06 13:54:10 +0100

    package demo

---
+++ day2/my_modules/myModule18/tools.py
@@ -0,0 +1,14 @@
+###################################################
+# My module !
+###################################################
+import numpy as np
+
+a = 5.
+b = 7.
+c = 12.
+
+def myFunc(x):
+    """
+    A function that returns sin(x)
+    """
+    return np.sin(x)
